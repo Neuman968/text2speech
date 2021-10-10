1f1756128bfc5459d4c2c8a8ecd7b58d49fa858c
johnflynn <johnflynn493@gmail.com>
2021-10-10 18:44:09 -0400

    Fixed enter callback.

--- src/SpeakCard.js
+++ src/SpeakCard.js
@@ -1,25 +1,37 @@
 import React from 'react'
 import {Button, Card, CardContent, TextareaAutosize} from "@mui/material";
 
-function SpeakCard({ voice }) {
+function SpeakCard({voice}) {
 
-    const [ message, setMessage ] = React.useState('')
+    const [message, setMessage] = React.useState('')
 
-    const speak = () => {
-        const utterance = new SpeechSynthesisUtterance(message)
+    const speak = (text) => {
+        const utterance = new SpeechSynthesisUtterance(text)
         utterance.voice = voice
         speechSynthesis.speak(utterance)
     }
+
+    // Speak on Enter press.
+    React.useEffect(() => {
+        const eventListener = (event) => {
+            if (event.key === 'Enter') {
+                event.preventDefault()
+                speak(message)
+            }
+        }
+        window.addEventListener('keydown', eventListener);
+        return () => window.removeEventListener('keydown', eventListener)
+    }, [message, voice]);
 
     return (<Card>
         <CardContent>
             <TextareaAutosize
                 minRows={10}
-                onChange={(e) => setMessage(() => e.target.value)}
-                style={{ width: 600 }}
+                onChange={(e) => setMessage(e.target.value)}
+                style={{width: 600}}
             />
             <Button
-                onClick={() => speak()}
+                onClick={() => speak(message)}
             >Speak</Button>
         </CardContent>
     </Card>)
