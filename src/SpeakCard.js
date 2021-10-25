c75a9952d10746fa4c8bf18daaa3c5819a4371da
johnflynn <johnflynn493@gmail.com>
2021-10-24 20:20:29 -0400

    Updated TextArea styling

--- src/SpeakCard.js
+++ src/SpeakCard.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import {Button, Card, CardContent, TextareaAutosize} from "@mui/material";
 import Box from "@mui/material/Box";
+import { useTheme } from '@mui/material/styles';
 
 function SpeakCard({ voice }) {
+
+    const theme = useTheme()
 
     const [message, setMessage] = React.useState('')
 
@@ -30,7 +33,11 @@ function SpeakCard({ voice }) {
                 <TextareaAutosize
                     minRows={10}
                     onChange={(e) => setMessage(e.target.value)}
-                    style={{width: 600}}
+                    style={{
+                        width: 600,
+                        backgroundColor: theme.palette.mode === 'dark' ? '#212121' : '#fff',
+                        color: theme.palette.mode === 'dark' ? 'white' : 'black'
+                    }}
                 />
                 <Button
                     variant="contained"
