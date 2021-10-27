6a775cdd4a7d3914774bb6506607e45dc6399ac0
johnflynn <johnflynn493@gmail.com>
2021-10-27 19:56:50 -0400

    Changed Info Modal to use Dialog.

--- src/InfoModal.js
+++ src/InfoModal.js
@@ -1,37 +1,39 @@
 import React from 'react'
-import Modal from "@mui/material/Modal";
+import Dialog from '@mui/material/Dialog';
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
+import {DialogContent} from "@mui/material";
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
 
 function InfoModal({ open, handleClose }) {
-    return (<Modal
+    return (<Dialog
+        onClose={handleClose}
         open={open}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-        handleClose={handleClose}
-        style={style}
     >
-        <Box>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
+        <DialogTitle>
+            About
+        </DialogTitle>
+        <DialogContent>
+            <Typography id="modal-modal-title" variant="p" component="h5">
                 Type in text in text box and press enter or speak button.
             </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-        </Box>
-    </Modal>)
+        </DialogContent>
+        <DialogActions>
+            <Button autoFocus onClick={handleClose}>Close</Button>
+        </DialogActions>
+        {/*<Box>*/}
+        {/*    <Typography id="modal-modal-title" variant="h6" component="h2">*/}
+        {/*        Type in text in text box and press enter or speak button.*/}
+        {/*    </Typography>*/}
+        {/*    <Typography id="modal-modal-description" sx={{ mt: 2 }}>*/}
+        {/*        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.*/}
+        {/*    </Typography>*/}
+        {/*</Box>*/}
+    </Dialog>)
 }
 
 export default InfoModal
